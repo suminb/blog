1a53cf4ccf64231748ec651791677c13dcff8a68
Sumin Byeon <suminb@gmail.com>
2016-11-06 23:11:02 +0900

    Publish generated static pages using a force push

--- publish.sh
+++ publish.sh
@@ -5,11 +5,12 @@ BUILD_DIR=_site
 LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
 rm -rf $BUILD_DIR
-git clone $TARGET_REPO $BUILD_DIR
 jekyll build
 pushd $BUILD_DIR
 rm publish.sh
+git init
+git remote add origin $TARGET_REPO
 git add .
 git commit -m "$LAST_COMMIT_MESSAGE"
-git push
+git push -f origin master
 popd
